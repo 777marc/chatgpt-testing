f56c38bd7cb08253afde063294db16406612b016
Marc Mendoza <777marc@gmail.com>
2023-10-31 09:31:53 -0400

    misc

--- index.js
+++ index.js
@@ -6,20 +6,19 @@ async function main() {
   const completion = await openai.chat.completions.create({
     messages: [
       {
-        role: "user",
-        content: "Find 10 star wars trivia questions in JSON format",
+        role: "system",
+        content: "Find 10 marine corps trivia questions in JSON format",
       },
     ],
     model: "gpt-3.5-turbo",
   });
 
-  console
   try {
-    //const contents = JSON.parse(completion.choices[0].message.content);
-    console.log(JSON.parse(completion.choices[0].message.content));
-    // JSON.parse(completion.choices[0].message.content).questions.forEach(function (item) {
-    //   console.log(item.question);
-    // });
+    const contents = JSON.parse(completion.choices[0].message.content);
+    //console.log(contents.questions);
+    contents.questions.forEach(function (item) {
+      console.table(item);
+    });
   } catch (error) {
     console.log("error:", error);
   }
